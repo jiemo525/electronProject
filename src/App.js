ef98fb4b4bead768fe4877b666a7b582e7eb1dee
gege.luo <godluo525@gmail.com>
2019-10-12 20:43:23 +0800

    electron 和react结合框架

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        electron react 
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
